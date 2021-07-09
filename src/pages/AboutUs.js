8ce64f902eee5c4036cc3cd0d4d67e504da19dd0
panos.mita2@outlook.com <pmita@dxc.com>
2021-07-09 17:09:48 +0100

    routing the pages

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUS = () => {
+    return(
+        <div>
+            <h1>Hello from AboutUS</h1>
+            <h1>Hello from AboutUS</h1>
+            <h1>Hello from AboutUS</h1>
+        </div>
+    )
+}
+
+export default AboutUS;
